14a30869f48d53508beb04c07a46a9dd6594efd2
sunnynehar56 <sunnynehar56@gmail.com>
2019-11-10 06:55:08 +0000

    inital commit

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -7,8 +7,8 @@ docker push sunnynehar56/tick
 docker volume create --name tick-data  -v /var/lib/influxdb/data -v /var/lib/influxdb/wal -v /var/lib/influxdb/meta 
       -v /data/kapacitor \
       -v /data/chronograf \
-      
-docker run -d -p 8186:8186 -p 8125:8125/udp -p 10000:10000 
+
+docker run -d -p 8186:8186 -p 8125:8125/udp -p 10000:10000 \
   --name tick \
   --volumes-from tick-data
   sunnynehar56/tick
