ab45213666022922e3629b27489cba3afe77aaab
sunnynehar56 <sunnynehar56@gmail.com>
2019-11-11 06:26:40 +0000

    Inital commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Data_container=$(docker ps -a | grep tick-data | egrep -o "[a-z0-9]{12}")
+VOLUME_CONTAINER_GUID=$(docker ps -a | grep tick-data | egrep -o "[a-z0-9]{12}")
 EXISTS=true
 TICK_RUNNING=$(docker ps -a | egrep "tick" | egrep -o "[a-z0-9]{12}")
 
@@ -7,29 +7,21 @@ if [ -n "${TICK_RUNNING}" ]; then
   docker rm -f tick
 fi
 
-if [ -z "$Data_container}" ]; then
+if [ -z "${VOLUME_CONTAINER_GUID}" ]; then
   EXISTS=false
-  Data_container=$(
+  VOLUME_CONTAINER_GUID=$(
     docker create \
       --name tick-data \
-      -v "/var/lib/influxdb/data" \
-      -v "/var/lib/influxdb/wal" \
-      -v "/var/lib/influxdb/meta" \
+      -v "/data/influx/data" \
+      -v "/data/influx/wal" \
+      -v "/data/influx/meta" \
       -v "/data/kapacitor" \
       -v "/data/chronograf" \
       sunnynehar56/tick \
       /dev/null
   )
-  echo ">> Created persisted data container: ${Data_container}"
+  echo ">> Created persisted data container: ${VOLUME_CONTAINER_GUID}"
 fi
-
-# docker create -v /var/lib/influxdb/data --name tick-data sunnynehar56/tick
-# docker create -v /var/lib/influxdb/wal --name tick-data sunnynehar56/tick --error
-# docker create -v /var/lib/influxdb/meta --name tick-data sunnynehar56/tick
-# docker create -v /data/kapacitor --name tick-data sunnynehar56/tick
-# docker create -v /data/chronograf --name tick-data sunnynehar56/tick
-
-# docker run -d -p 8186:8186 -p 8125:8125/udp -p 10000:10000 --name tick --volumes-from tick-data sunnynehar56/tick
 
 docker run \
   -d \
@@ -37,12 +29,25 @@ docker run \
   -p 8125:8125/udp \
   -p 10000:10000 \
   --name tick \
-  --volumes-from $Data_container \
+  --volumes-from $VOLUME_CONTAINER_GUID \
   sunnynehar56/tick
 
-EXISTS=true
 HOST=$(docker-machine env dev | grep DOCKER_HOST | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 echo ">> Waiting for Influx to be available on active Docker Host (${HOST})"
+if command -v nc 2>&1> /dev/null; then
+  WAIT=0
+  while ! nc -z $HOST 8086; do
+    sleep 1
+    WAIT=$(($WAIT + 1))
+    if [ "$WAIT" -gt 15 ]; then
+      echo "Error: Timeout wating for Influx to start"
+      exit 1
+    fi
+  done
+else
+  echo "NOTE: nc (netcat) not deteced, using arbitrary sleep of 15s"
+  sleep 15
+fi
 
 if [ $EXISTS != true ]; then
   echo ">> Creating initial telegraf database"
@@ -51,5 +56,5 @@ fi
 
 if [[ $? -eq 0 ]]; then
   echo ""
-  echo "You are ready to go, head on over to ${HOST}:10000 and start creating metrics!"
+  echo
 fi
