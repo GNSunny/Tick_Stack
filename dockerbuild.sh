cecb29cdbad1f233bda831fd1027f81b8c79bd63
sunnynehar56 <sunnynehar56@gmail.com>
2019-11-11 03:11:51 +0000

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -14,3 +14,17 @@ docker run -d -p 8186:8186 -p 8125:8125/udp -p 10000:10000 \
   --name tick \
   --volumes-from tick-data \
   sunnynehar56/tick
+
+EXISTS=true
+HOST=$(docker-machine env aws-sandbox | grep DOCKER_HOST | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+echo ">> Waiting for Influx to be available on active Docker Host (${HOST})"
+
+if [ $EXISTS != true ]; then
+  echo ">> Creating initial telegraf database"
+  docker exec -i -t tick curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE telegraf"
+fi
+
+if [[ $? -eq 0 ]]; then
+  echo ""
+  echo "You are ready to go, head on over to ${HOST}:10000 and start creating metrics!"
+fi
